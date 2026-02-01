730d24571a2df1b5d8f1969a53db9289ffc74e8a
khaled955 <khaledmansour104@yahoo.com>
2026-02-02 00:53:00 +0300

    input

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -52,80 +51,20 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
   VariantProps<typeof buttonVariants> {
   asChild?: boolean
-=======
-import * as React from 'react';
-import { Slot } from '@radix-ui/react-slot';
-import { cva, type VariantProps } from 'class-variance-authority';
-import { cn } from '@/lib/utility/tailwind-merge';
-
-const buttonVariants = cva(
-  'h-14 w-44 px-3 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
-  {
-    variants: {
-      variant: {
-        primary:
-          'bg-maroon-600  text-white  hover:bg-maroon-700 disabled:bg-zinc-300 disabled:text-zinc-500 dark:bg-softpink-300 dark:hover:bg-softpink-400 dark:text-zinc-800 dark:disabled:bg-zinc-700 dark:disabled:text-zinc-600',
-        destructive:
-          'bg-red-600 text-white hover:bg-red-700 disabled:bg-zinc-300 disabled:text-zinc-500 dark:bg-red-500 dark:hover:bg-red-600 dark:text-zinc-50 dark:disabled:bg-zinc-700 dark:disabled:text-zinc-600',
-        Outline:
-          'border border-maroon-600 text-maroon-600  hover:bg-maroon-50 disabled:bg-zinc-100 disabled:border-zinc-300 disabled:text-zinc-400 dark:border-softpink-300 dark:text-softpink-300 dark:bg-zinc-800 dark:hover:bg-zinc-700 dark:disabled:bg-zinc-800 dark:disabled:text-zinc-600  dark:disabled:border-zinc-600',
-        Subtle:
-          'bg-zinc-50 border border-zinc-400 text-zinc-800  hover:bg-zinc-100 disabled:bg-zinc-100 disabled:border-zinc-300 disabled:text-zinc-400 dark:bg-zinc-800 dark:border-zinc-500 dark:text-zinc-50 dark:hover:bg-zinc-700  dark:disabled:bg-zinc-700 dark:disabled:text-zinc-600 dark:disabled:border-zinc-600',
-        secondary:
-          'bg-maroon-50 text-maroon-600  hover:bg-maroon-100 disabled:bg-zinc-300 disabled:text-zinc-500 dark:bg-zinc-700 dark:text-softpink-300 dark:hover:bg-zinc-600 dark:disabled:bg-zinc-700 dark:disabled:text-zinc-600',
-        ghost:
-          'hover:bg-zinc-100 text-zinc-800 data-[state=open]:bg-zinc-10 hover:text-zinc-800 disabled:text-zinc-400 dark:bg-transparent dark:hover:bg-zinc-700 dark:hover:text-zinc-50 dark:text-zinc-50 dark:disabled:text-zinc-600 dark:disabled:bg-zinc-700',
-        link: 'text-primary underline-offset-4 hover:underline',
-        carousel: '',
-      },
-      size: {
-        default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
-        icon: 'h-9 w-9',
-      },
-    },
-    defaultVariants: {
-      variant: 'primary',
-      size: 'default',
-    },
-  },
-);
-
-export interface ButtonProps
-  extends
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-  isLoading?: boolean;
->>>>>>> 436290dfbab3383191171ad2cd5f649972267e00
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-<<<<<<< HEAD
     const Comp = asChild ? Slot : "button"
-=======
-    const Comp = asChild ? Slot : 'button';
->>>>>>> 436290dfbab3383191171ad2cd5f649972267e00
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-<<<<<<< HEAD
     )
   }
 )
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
-=======
-    );
-  },
-);
-Button.displayName = 'Button';
-
-export { Button, buttonVariants };
->>>>>>> 436290dfbab3383191171ad2cd5f649972267e00
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { cn } from '@/lib/utility/tailwind-merge';
+import { cn } from '@/lib/utility/cn.util';
 import { Search } from 'lucide-react';
-
 type InputVariant =
   | 'text'
   | 'email'
@@ -11,19 +10,15 @@ type InputVariant =
   | 'file'
   | 'tel'
   | 'url';
-
 interface InputProps extends React.ComponentProps<'input'> {
   variant?: InputVariant;
   error?: boolean;
 }
-
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, variant = 'text', error, disabled, ...props }, ref) => {
     return (
       <div className="relative">
-        <InputField
-          className="pe-12"
-          {...props}
+        <input
           ref={ref}
           type={type ?? variant}
           data-error={error ? 'true' : 'false'}
@@ -31,46 +26,34 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           className={cn(
             /* ================= Base ================= */
             'flex h-12 w-80 rounded-lg border border-zinc-300 bg-transparent p-4 text-base text-zinc-800 transition-colors dark:border-zinc-600 dark:bg-zinc-700 dark:text-zinc-400 dark:hover:border-zinc-500 dark:focus:border-softpink-400 md:text-sm',
-
             /* ================= Default ================= */
             'border-zinc-300 placeholder:text-muted-foreground',
-
             /* ================= Hover ================= */
             'hover:border-zinc-400',
-
             /* ================= Focus ================= */
             'focus:border-maroon-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-maroon-600',
-
             /* ================= Error (highest priority) ================= */
             'data-[error=true]:border-red-600',
             'data-[error=true]:focus:border-red-600',
             'data-[error=true]:focus-visible:ring-red-600',
-
             /* ================= Disabled ================= */
             'disabled:cursor-not-allowed disabled:border-0 disabled:bg-zinc-100 disabled:opacity-50',
-
-          /* ================= Variants ================= */
-
+            /* ================= Variants ================= */
             // Text / Email / URL / Tel
             (variant === 'text' ||
               variant === 'email' ||
               variant === 'url' ||
               variant === 'tel') &&
               'text-zinc-500',
-
             // Password
             variant === 'password' && 'pr-10 text-zinc-400',
-
             // Number
             variant === 'number' && 'appearance-none',
-
             // Search
             variant === 'search' && 'pl-10',
-
             // File
             variant === 'file' &&
               'file file:cursor-pointer file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-maroon-500',
-
             className,
           )}
           {...props}
@@ -87,7 +70,5 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     );
   },
 );
-
 Input.displayName = 'Input';
-
 export { Input };
