60530bee63cc29d363e697ede37329f907ac91dd
ahmedmitwally77 <anaahmedmito5@gmail.com>
2026-02-06 23:41:56 +0200

    refactor(home): remove unused Header and Footer components from the Home page

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -5,13 +5,10 @@ import TestimonialsCarousel from './_components/testimonials';
 import Hero from './_components/hero';
 import OccasionSection from './_components/occasion-section';
 import HeroServiceSection from './_components/hero-service-section';
-import Header from '@/components/layout/app/header';
-import Footer from '@/components/layout/app/footer';
 
 export default function Home() {
   return (
     <main className="mx-auto flex flex-col items-center justify-center">
-      <Header />
       {/* Hero Section */}
       <Hero />
 
@@ -24,7 +21,6 @@ export default function Home() {
       <Gallery />
       <TestimonialsCarousel />
       <Companies />
-      <Footer />
     </main>
   );
 }
