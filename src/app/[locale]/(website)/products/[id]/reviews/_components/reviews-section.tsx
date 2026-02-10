a2f4ddff0cb9531074832119d5855cecfbe0333f
Mostafa3lyan <96500374+Mostafa3lyan@users.noreply.github.com>
2026-02-10 21:07:50 +0200

    chore: remove package-lock

--- src/app/[locale]/(website)/products/[id]/reviews/_components/reviews-section.tsx
+++ src/app/[locale]/(website)/products/[id]/reviews/_components/reviews-section.tsx
@@ -4,6 +4,7 @@ import RelatedProducts from './related-products';
 type ReviewsSectionProps = {
   productId: string;
 };
+
 export default function ReviewsSection({ productId }: ReviewsSectionProps) {
   return (
     <section className="container mx-auto flex w-11/12 flex-col py-10">
