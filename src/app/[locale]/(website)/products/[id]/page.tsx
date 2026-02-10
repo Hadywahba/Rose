d4d14d457418449c5f07d22df165eb2458f34fda
Mostafa3lyan <96500374+Mostafa3lyan@users.noreply.github.com>
2026-02-10 19:29:51 +0200

    refactor(reviews): added Todo comment for teammate

--- src/app/[locale]/(website)/products/[id]/page.tsx
+++ src/app/[locale]/(website)/products/[id]/page.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReviewsSection from './reviews/_components/reviews-section';
 
 export default function page() {
+  // TODO: Get this productId dynamically based on the product page
+  const productId = '673e2e1f1159920171828153';
   return (
     <>
-      <ReviewsSection />
+      <ReviewsSection productId={productId} />
     </>
   );
 }
--- src/app/[locale]/(website)/products/[id]/reviews/_components/reviews-section.tsx
+++ src/app/[locale]/(website)/products/[id]/reviews/_components/reviews-section.tsx
@@ -1,10 +1,10 @@
 import { ProductsReviews } from './product-reviews';
 import RelatedProducts from './related-products';
 
-export default function ReviewsSection() {
-  // TODO: Get this productId dynamically based on the product page
-  const productId = '673e2e1f1159920171828153';
-  
+type ReviewsSectionProps = {
+  productId: string;
+};
+export default function ReviewsSection({ productId }: ReviewsSectionProps) {
   return (
     <section className="container mx-auto flex w-11/12 flex-col py-10">
       <ProductsReviews productId={productId} />
