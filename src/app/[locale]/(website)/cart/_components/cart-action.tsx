85416141420d14228349b03dee64e2c917f9b106
khaled955 <khaledmansour104@yahoo.com>
2026-02-14 12:33:05 +0300

    enhance cart item ui

--- src/app/[locale]/(website)/cart/_components/cart-action.tsx
+++ src/app/[locale]/(website)/cart/_components/cart-action.tsx
@@ -1,17 +1,17 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useDeleteProductFromCart } from "@/lib/hooks/cart/use-delete-product-from-cart";
-import { useGuestCartContext } from "@/lib/hooks/cart/use-guest-cart-context";
-import { useUpdateCart } from "@/lib/hooks/cart/use-update-cart";
-import { useValidateCartInput } from "@/lib/hooks/cart/use-validate-cart-input";
-import { Minus, Plus, Trash2 } from "lucide-react";
-import { useSession } from "next-auth/react";
-import { useTranslations } from "next-intl";
-import { useEffect, useRef, useState } from "react";
-import { toast } from "sonner";
-import { useDebouncedCallback } from "use-debounce";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useDeleteProductFromCart } from '@/lib/hooks/cart/use-delete-product-from-cart';
+import { useGuestCartContext } from '@/lib/hooks/cart/use-guest-cart-context';
+import { useUpdateCart } from '@/lib/hooks/cart/use-update-cart';
+import { useValidateCartInput } from '@/lib/hooks/cart/use-validate-cart-input';
+import { Minus, Plus, Trash2 } from 'lucide-react';
+import { useSession } from 'next-auth/react';
+import { useTranslations } from 'next-intl';
+import { useEffect, useRef, useState } from 'react';
+import { toast } from 'sonner';
+import { useDebouncedCallback } from 'use-debounce';
 
 type CartActionProps = {
   productId: string;
@@ -55,9 +55,9 @@ export default function CartAction({
 
   // Variables
   const value = Number(count);
-  const isDecButtonDisabled = count.trim() === "" || value === 1 || isPending;
+  const isDecButtonDisabled = count.trim() === '' || value === 1 || isPending;
   const isIncButtonDisabled =
-    count.trim() === "" || value >= productQuantity || isPending;
+    count.trim() === '' || value >= productQuantity || isPending;
 
   // ONE debounced function for both cases
   const debouncedUpdateCart = useDebouncedCallback((nextValue: number) => {
@@ -106,7 +106,7 @@ export default function CartAction({
 
     const nextError = applyValidation(next);
 
-    if (nextError || next.trim() === "") {
+    if (nextError || next.trim() === '') {
       debouncedUpdateCart.cancel();
       return;
     }
@@ -131,48 +131,52 @@ export default function CartAction({
   }
 
   return (
-    <div className="cart-actions flex flex-col gap-3 w-full md:w-[40%] md:justify-between">
+    <div className="cart-actions flex w-full flex-col items-end gap-3 md:w-[40%] md:justify-between">
       {/* remove-product-from-cart */}
       <Button
         disabled={deleteProductIsPending}
         onClick={handleDeleteProduct}
-        className="self-center md:self-end bg-red-600 hover:bg-red-700 md:w-fit md:px-2 md:py-4"
+        className="self-center bg-red-600 hover:bg-red-700 md:w-fit md:self-end md:px-2 md:py-4"
       >
         <Trash2 /> <span className="capitalize">{t('remove')}</span>
       </Button>
+      <div className='mb-4 space-y-3'>
+        <div className="update-btn flex gap-2">
+          <Button
+            className="w-fit p-6"
+            variant={'secondary'}
+            disabled={isDecButtonDisabled}
+            onClick={handleDecreaseQuantity}
+          >
+            <Minus />
+          </Button>
 
-      <div className="update-btn flex gap-2 items-center justify-between md:justify-start">
-        <Button
-          className="w-fit p-6"
-          variant={"secondary"}
-          disabled={isDecButtonDisabled}
-          onClick={handleDecreaseQuantity}
-        >
-          <Minus />
-        </Button>
+          <Input
+            className="w-fit text-center"
+            placeholder="1"
+            type="number"
+            value={count}
+            onChange={handleChangeCount}
+            disabled={isPending}
+          />
 
-        <Input
-          className="w-16 text-center md:grow"
-          placeholder="1"
-          type="number"
-          value={count}
-          onChange={handleChangeCount}
-          disabled={isPending}
-        />
+          <Button
+            className="w-fit p-6"
+            variant={'secondary'}
+            disabled={isIncButtonDisabled}
+            onClick={handleIncreaseQuantity}
+          >
+            <Plus />
+          </Button>
+        </div>
 
-        <Button
-          className="w-fit p-6"
-          variant={"secondary"}
-          disabled={isIncButtonDisabled}
-          onClick={handleIncreaseQuantity}
-        >
-          <Plus />
-        </Button>
+        {/* validation-error */}
+        {error && (
+          <p className="self-center text-center text-sm capitalize text-red-500">
+            {error}
+          </p>
+        )}
       </div>
-
-      {error && (
-        <p className="capitalize text-sm text-red-500 text-center">{error}</p>
-      )}
     </div>
   );
 }
--- src/app/[locale]/(website)/cart/_components/cart-item.tsx
+++ src/app/[locale]/(website)/cart/_components/cart-item.tsx
@@ -98,6 +98,8 @@ export default function CartItem({
         productQuantity={productQuantity}
         quantityInCart={quantity}
       />
+
+      
     </div>
   );
 }
