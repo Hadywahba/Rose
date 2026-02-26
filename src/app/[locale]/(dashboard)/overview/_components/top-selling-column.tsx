05dfe0dbdc7efb0f7ae9579883120f25ee6f2334
Mostafa3lyan <96500374+Mostafa3lyan@users.noreply.github.com>
2026-02-27 00:47:46 +0200

    feat(3rd-section): top selling initial ui

--- src/app/[locale]/(dashboard)/overview/_components/top-selling-column.tsx
+++ src/app/[locale]/(dashboard)/overview/_components/top-selling-column.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 export default function TopSellingColumn() {
   return (
-    <div>TopSellingColumn</div>
-  )
+    <div className="flex flex-col gap-6">
+      <p className="text-2xl font-semibold text-zinc-800">
+        Top Selling Products
+      </p>
+      <div className="column flex flex-col gap-3">
+        <div className="flex items-center justify-between rounded bg-zinc-100 p-2">
+          <p>
+            25 Red Roses | Black Wrap <span>(1,999 EGP)</span>
+          </p>
+          <p>
+            5011 <span>Sales</span>
+          </p>
+        </div>
+        <div className="flex items-center justify-between bg-zinc-100 p-2">
+          <span>25 Red Roses | Black Wrap (1,999 EGP)</span>
+          <span>5011 Sales</span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/[locale]/(dashboard)/overview/_components/top-selling-section.tsx
+++ src/app/[locale]/(dashboard)/overview/_components/top-selling-section.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import LowStockColumn from './low-stock-column'
-import TopSellingColumn from './top-selling-column'
+import React from 'react';
+import LowStockColumn from './low-stock-column';
+import TopSellingColumn from './top-selling-column';
 
 export default function TopSellingSection() {
   return (
-    <section>
-      <TopSellingColumn />
-      <LowStockColumn/>
-    </section>
-  )
+    <div className="fixed bottom-0 right-0 h-[440px] w-3/4 bg-red-50">
+      <section className='p-6 grid grid-cols-2 gap-6'>
+        <TopSellingColumn />
+        <LowStockColumn />
+      </section>
+    </div>
+  );
 }
