15fb8e97b7533b8c0ad7059a858ee38637fb2f42
Mostafa3lyan <96500374+Mostafa3lyan@users.noreply.github.com>
2026-02-27 02:55:48 +0200

    feat(3rd-section): top selling retrieve function and types files refactor

---
+++ src/lib/services/dashboard/get-top-products.service.ts
@@ -0,0 +1,16 @@
+import { JSON_HEADER } from "@/lib/constants/api.constant";
+import { TopProductsResponse } from "@/lib/types/products/product";
+
+export const getTopProducts = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API}/products?fields=title,sold,quantity,price`,
+    {
+      headers: { ...JSON_HEADER },
+    },
+  );
+  const payload: TopProductsResponse = await response.json();
+  console.log(payload);
+  
+  
+  return payload;
+};
--- src/lib/types/products/product.d.ts
+++ src/lib/types/products/product.d.ts
@@ -1,4 +1,4 @@
-export type Product = {
+export interface Product {
   _id: string;
   title: string;
   slug: string;
@@ -19,4 +19,20 @@ export type Product = {
   rateCount: number;
   favoriteId: string | null;
   isInWishlist: boolean;
-};
+}
+
+export interface Metadata {
+  currentPage: number;
+  limit: number;
+  totalPages: number;
+  totalItems: number;
+}
+
+export interface ProductsResponse {
+  metadata: Metadata;
+  products: Product[];
+}
+
+export interface TopProductsResponse {
+  product: Pick<Product, "_id" | "title" | "sold" | "quantity" | "price">;
+}
