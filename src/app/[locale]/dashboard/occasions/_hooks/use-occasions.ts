deae6c6bba6cef7911ca3df0ff7655bd2094b1bc
hady wahba <157915807+Hadywahba@users.noreply.github.com>
2026-03-04 13:26:22 +0200

    feat(dashboard-occasion):add occasion hook

---
+++ src/app/[locale]/dashboard/occasions/_hooks/use-occasions.ts
@@ -0,0 +1,26 @@
+import { getOccasion } from '@/lib/services/occasion/occasion-content.service';
+import { useQuery } from '@tanstack/react-query';
+
+export const useOccasion = (
+  limit: number,
+  page: number
+) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['Occasion', limit, page],
+    queryFn: async () => {
+      const payload = await getOccasion(limit, page);
+
+      if ('error' in payload) {
+        throw new Error(payload.error);
+      }
+
+      return payload;
+    },
+  });
+
+  return {
+    occasion: data,
+    isLoading,
+    error,
+  };
+};
