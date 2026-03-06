05cbc4c362d688ed785733c60c715071ec354ba6
Mostafa3lyan <96500374+Mostafa3lyan@users.noreply.github.com>
2026-03-06 08:10:25 +0200

    refactor(3rd-section): add Suspense and skeleton

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import TopSellingSection from './_components/top-selling-section'
+import { Suspense } from 'react';
+import TopSellingSection from './_components/top-selling-section';
+import TopSellingSectionSkeleton from './_components/top-selling-section-skeleton';
 
 export default function page() {
   return (
-    <>page
-      <TopSellingSection />
+    <>
+      <Suspense fallback={<TopSellingSectionSkeleton />}>
+        <TopSellingSection />
+      </Suspense>
     </>
-  )
+  );
 }
