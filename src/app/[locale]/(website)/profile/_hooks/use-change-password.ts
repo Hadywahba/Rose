a4986d4baaef588b0694c43c7985a5c3db527676
hady wahba <157915807+Hadywahba@users.noreply.github.com>
2026-04-28 11:52:27 +0300

    feat:add change password hook

---
+++ src/app/[locale]/(website)/profile/_hooks/use-change-password.ts
@@ -0,0 +1,26 @@
+'use client';
+
+import { changePasswordProfile } from '@/lib/actions/profile/change-password-profile.action';
+import { ChangePasswordFormFields } from '@/lib/schema/profile/change-password.schema';
+import { useMutation } from '@tanstack/react-query';
+
+export const useUpdateProfile = () => {
+  // Mutations
+  const { mutate, isPending, error } = useMutation({
+    mutationFn: async (fields: ChangePasswordFormFields) => {
+      const payload = await changePasswordProfile(fields);
+
+      if (payload.status === false) {
+        throw new Error(payload.message);
+      }
+
+      return payload;
+    },
+  });
+
+  return {
+    ChangePassword: mutate,
+    isPending,
+    error,
+  };
+};
